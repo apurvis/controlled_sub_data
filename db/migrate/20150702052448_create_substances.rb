f54c562758b8ea32049662f661779965191b891b
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-07-02 08:11:19 +0200

    Comments

--- db/migrate/20150702052448_create_substances.rb
+++ db/migrate/20150702052448_create_substances.rb
@@ -3,7 +3,7 @@ class CreateSubstances < ActiveRecord::Migration
     create_table :substances do |t|
       t.string :name
       t.string :classification, comment: "e.g. opioid, stimulant, depressant"
-      t.string :chemical_formula
+      t.string :chemical_formula, comment: "In a readable format, e.g. N-ethyl-3-piperidyl benzilat"
       t.string :chemical_formula_smiles_format, comment: "Follows the SMILE standard: https://en.wikipedia.org/wiki/Simplified_molecular-input_line-entry_system"
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20150702053128) do
   create_table "substances", force: :cascade do |t|
     t.string   "name"
     t.string   "classification",                              comment: "e.g. opioid, stimulant, depressant"
-    t.string   "chemical_formula"
+    t.string   "chemical_formula",                            comment: "In a readable format, e.g. N-ethyl-3-piperidyl benzilat"
     t.string   "chemical_formula_smiles_format",              comment: "Follows the SMILE standard: https://en.wikipedia.org/wiki/Simplified_molecular-input_line-entry_system"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
