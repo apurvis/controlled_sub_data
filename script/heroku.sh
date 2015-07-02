a84b5e1e470ac70a997e029a212e370a54ee9e90
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-07-02 18:34:24 +0200

    script

--- script/heroku.sh
+++ script/heroku.sh
@@ -2,7 +2,7 @@
 pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start
 
 # push dev DB to production
-heroku pg:reset DATABASE_URL
+heroku pg:reset DATABASE_URL --confirm controlled-sub-data
 heroku pg:push controlled_sub_data_development DATABASE_URL --app controlled-sub-data
 
 # Connect to psql
