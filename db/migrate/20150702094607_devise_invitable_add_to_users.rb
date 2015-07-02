6e3699ed9d999806295ab6b45447398ecd38f92e
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-07-02 11:54:49 +0200

    Fix devise migration

--- db/migrate/20150702094607_devise_invitable_add_to_users.rb
+++ db/migrate/20150702094607_devise_invitable_add_to_users.rb
@@ -8,5 +8,8 @@ class DeviseInvitableAddToUsers < ActiveRecord::Migration
     add_column :users, :invited_by_id, :integer
     add_column :users, :invited_by_type, :string
     add_index :users, :invitation_token, :unique => true
+
+    # Allow null encrypted_password
+    change_column :users, :encrypted_password, :string, :null => true
   end
 end
