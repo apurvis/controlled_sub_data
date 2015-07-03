f3020adea1b6d2120e4162859a3e3fe423cb9a6e
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-07-03 09:37:35 +0200

    few more data tweaks; add barbital

--- script/add_alternate_names.rb
+++ script/add_alternate_names.rb
@@ -1,2 +1,5 @@
 s = Substance.where(name: 'Phencyclidine').first
 SubstanceAlternateName.create(substance_id: s.id, name: 'PCP')
+
+s = Substance.where(name: 'Methylphenobarbital').first
+SubstanceAlternateName.create(substance_id: s.id, name: 'Mephobarbital')
