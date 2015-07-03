88b63eea7a58f0b8aa5bcbb1187989a50dab0549
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-07-03 10:08:41 +0200

    Add manual nabilone

--- script/add_alternate_names.rb
+++ script/add_alternate_names.rb
@@ -3,3 +3,13 @@ SubstanceAlternateName.create(substance_id: s.id, name: 'PCP')
 
 s = Substance.where(name: 'Methylphenobarbital').first
 SubstanceAlternateName.create(substance_id: s.id, name: 'Mephobarbital')
+
+s = Substance.where(name: 'Dimethyltryptamine').first
+SubstanceAlternateName.create(substance_id: s.id, name: 'DMT')
+
+s = Substance.where(name: 'Diethyltryptamine').first
+SubstanceAlternateName.create(substance_id: s.id, name: 'DMT')
+
+s = Substance.where(name: 'Nabilone').first
+s.first_scheduled_date = "1987-04-07".to_date
+s.save
