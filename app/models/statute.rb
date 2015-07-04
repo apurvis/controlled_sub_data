e7141e755130efb16084b6f2ec0b2f048f906301
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-07-04 05:10:26 +0200

    Make the states a drop down instead of text

--- app/models/statute.rb
+++ app/models/statute.rb
@@ -6,6 +6,8 @@ class Statute < ActiveRecord::Base
   validates :state, presence: true, length: { minimum: 2 }
   validates :start_date, presence: true
 
+  STATES = ['FEDERAL', 'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+
   def formatted_name
     if start_date
       "#{state}/#{start_date.strftime('%Y-%m-%d')}"
@@ -15,6 +17,7 @@ class Statute < ActiveRecord::Base
   end
 
   private
+
   def has_valid_start_date
     begin
       start_date.to_date
