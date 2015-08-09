95242f6cf50e93443f940376f0be3e88c69b132a
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-08-09 20:37:01 +0200

    Statute searching

--- app/controllers/substance_statutes_controller.rb
+++ app/controllers/substance_statutes_controller.rb
@@ -55,6 +55,15 @@ class SubstanceStatutesController < ApplicationController
       :include_isomers,
       :include_optical_isomers,
       :include_geometric_isomers
+
+      :include_positional_isomers,
+      :include_salts_of_isomers,
+      :include_salts_of_optical_isomers,
+      :include_esters,
+      :include_ethers,
+      :include_compounds,
+      :include_materials,
+      :include_preparations.
     )
   end
 end
