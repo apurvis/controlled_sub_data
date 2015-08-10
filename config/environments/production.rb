51957b59d75602df9ab1193fa93706a71f0b9653
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-08-10 19:07:02 +0200

    remove junk

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,9 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => 'portal.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
