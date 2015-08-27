7dfbce79134eccc1e98bbf57bd4668f4f71b17f1
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-08-27 06:41:23 -0400

    Fix substance sort, classifications view.  add molecule to substance view

--- app/controllers/substances_controller.rb
+++ app/controllers/substances_controller.rb
@@ -3,7 +3,7 @@ class SubstancesController < ApplicationController
   before_action :vip_only, except: [:index, :show]
 
   def index
-    @substances = Substance.order('name ASC').paginate(page: params[:page])
+    @substances = Substance.order('LOWER(name) ASC').paginate(page: params[:page])
   end
 
   def show
