e24c57ab0d8a27e4a6acd983839a59637197bbc3
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-08-31 20:52:16 -0400

    Effective substance statutes

--- app/models/statute.rb
+++ app/models/statute.rb
@@ -13,14 +13,18 @@ class Statute < ActiveRecord::Base
   STATES = [FEDERAL, 'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 
   def effective_substance_statutes(options = {})
-    substance_statutes = substance_statutes + duplicated_federal_substance_statutes(options)
-    substance_statutes += @statute.statute_amendments.map { |amendment| amendment.substance_statutes }.flatten
-    substance_statutes.reject { |ss| substance_statutes.any? { |s| s.is_expiration? && ss.substance_id == s.substance_id } }
+    regulations = substance_statutes + duplicated_federal_substance_statutes(options)
+    regulations += statute_amendments.select { |a| !options[:as_of] || a.start_date <= options[:as_of] }
+                                            .map { |a| a.substance_statutes }
+                                            .flatten
+    regulations.reject { |ss| regulations.any? { |s| s.is_expiration? && ss.substance_id == s.substance_id && s.statute.start_date > ss.statute.start_date } }
+               .reject { |ss| ss.is_expiration? }
   end
 
   def duplicated_federal_substance_statutes(options = {})
-    substance_statutes = duplicated_federal_statutes(options).map { |s| s.substance_statutes }.flatten.compact
-    substance_statutes.reject { |ss| substance_statutes.any? { |s| s.is_expiration? && ss.substance_id == s.substance_id } }
+    regulations = duplicated_federal_statutes(options).map { |s| s.substance_statutes }.flatten.compact
+    regulations.reject { |ss| regulations.any? { |s| s.is_expiration? && ss.substance_id == s.substance_id } }
+               .reject { |ss| ss.is_expiration? }
   end
 
   def duplicated_federal_statutes(options = {})
--- spec/models/statute_spec.rb
+++ spec/models/statute_spec.rb
@@ -10,30 +10,38 @@ describe Statute do
     expect(inheriting_statute.duplicated_federal_substance_statutes.size).to eq(1)
   end
 
-  context 'substance_statutes' do
+  context 'duplicated federal' do
     it 'should find duplicated substance_statutes' do
       expect(inheriting_statute.duplicated_federal_substance_statutes).to eq(federal_statute.substance_statutes)
     end
 
     context 'expired on federal' do
-      let(:federal_amendment) { StatuteAmendment.new(state: federal_statute.state, start_date: inheritance_date - 1.year, parent_id: federal_statute.id) }
+      let(:federal_amendment) { StatuteAmendment.create(state: federal_statute.state, start_date: inheritance_date - 1.year, parent_id: federal_statute.id) }
       let!(:expiration) { SubstanceStatute.create(statute: federal_amendment, substance: federal_statute.substance_statutes.first.substance, is_expiration: true) }
 
-      it 'should exclude regulations that have expired on the federal statue' do
+      it 'should exclude regulations that have expired on the federal statute before the inheritance date' do
         expect(inheriting_statute.duplicated_federal_substance_statutes).to eq([])
       end
 
-      it 'should still find them if they expired after the cutoff' do
+      it 'should still find them if they expired after the request cutoff or after duplication date' do
         federal_amendment.start_date = inheritance_date + 1.year
         federal_amendment.save
         expect(inheriting_statute.duplicated_federal_substance_statutes(as_of: inheritance_date)).to eq([federal_statute.substance_statutes.first])
+        expect(inheriting_statute.duplicated_federal_substance_statutes).to eq([federal_statute.substance_statutes.first])
       end
     end
 
-    it 'should exclude regulations that have expired in the local amendments' do
-    end
+    context 'effective regulations as of date' do
+      let(:state_amendment) { StatuteAmendment.create(state: inheriting_statute.state, start_date: inheritance_date + 1.year, parent_id: inheriting_statute.id) }
+      let!(:expiration) { SubstanceStatute.create(statute: state_amendment, substance: federal_statute.substance_statutes.first.substance, is_expiration: true) }
 
-    it 'should include expired statutes if looking before their expiration date' do
+      it 'should exclude regulations that have expired in the local amendments' do
+        expect(inheriting_statute.effective_substance_statutes).to eq([inheriting_statute.substance_statutes.first])
+      end
+
+      it 'should include expired statutes if looking before their expiration date' do
+        expect(inheriting_statute.effective_substance_statutes(as_of: inheritance_date)).to eq([inheriting_statute.substance_statutes.first, federal_statute.substance_statutes.first])
+      end
     end
   end
 end
