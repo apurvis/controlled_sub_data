16ad2b5d5f95888533067332c27aae69b7fb9d2b
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-10-07 02:57:12 -0400

    Audits admin only

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -1,6 +1,6 @@
 class AuditsController < ApplicationController
   before_action :authenticate_user!
-  before_action :admin_only, :except => :show
+  before_action :admin_only
 
   def index
     @audits = Audit.order('created_at DESC').paginate(page: params[:page])
