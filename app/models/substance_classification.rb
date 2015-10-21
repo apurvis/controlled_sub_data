7f0290d2cb9d2cfacf3ba0a56a0831187703ff0a
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-10-21 16:13:15 -0400

    Color pink when no classification is given

--- app/models/substance_classification.rb
+++ app/models/substance_classification.rb
@@ -8,6 +8,6 @@ class SubstanceClassification < ActiveRecord::Base
   validates_uniqueness_of :name
 
   def substances
-    substance_statutes.map { |ss| ss.substance }.uniq
+    substance_statutes.map { |ss| ss.substance }.uniq.sort { |a,b| a.name <=> b.name }
   end
 end
