0dccefbbedba1385574c7ec57ea4e47bdf40ee4a
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2015-10-27 14:51:55 -0400

    Allow my audits

--- app/controllers/audits_controller.rb
+++ app/controllers/audits_controller.rb
@@ -3,6 +3,6 @@ class AuditsController < ApplicationController
   before_action :admin_only
 
   def index
-    @audits = Audited::Adapters::ActiveRecord::Audit.where('user_id <> 8').reorder('created_at DESC').paginate(page: params[:page])
+    @audits = Audited::Adapters::ActiveRecord::Audit.reorder('created_at DESC').paginate(page: params[:page])
   end
 end
