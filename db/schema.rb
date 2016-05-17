b625714af68e1b11644a1a8247ccc8476ad29e37
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2016-05-17 12:04:11 +0800

    Use acts_as_paranoid for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160511040138) do
+ActiveRecord::Schema.define(version: 20160517035030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -174,6 +174,7 @@ ActiveRecord::Schema.define(version: 20160511040138) do
     t.integer  "invitation_limit"
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
+    t.datetime "deleted_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
