52f1d0ed2bab01830521328fc4061a9e704ce488
apurvis@lumoslabs.com <apurvis@lumoslabs.com>
2016-07-22 02:28:58 -0400

    Sort the schedule level view

--- app/controllers/schedule_levels_controller.rb
+++ app/controllers/schedule_levels_controller.rb
@@ -13,7 +13,7 @@ class ScheduleLevelsController < ApplicationController
   def show
     @level = params['id'].to_i
     @roman_level = ScheduleLevelsController::LEVELS.keys[@level - 1]
-    @substances = SubstanceStatute.where(schedule_level: @level).map { |ss| ss.substance }
+    @substances = SubstanceStatute.where(schedule_level: @level).map { |ss| ss.substance }.uniq.sort { |a,b| a.to_s <=> b.to_s }
     @classifications = SubstanceClassification.where(schedule_level: @level).order(name: :asc)
   end
 
