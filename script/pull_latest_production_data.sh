1d928e990363fb644e943fdd0f4526699096f692
apurvis <apurvis@users.noreply.github.com>
2019-09-22 15:40:58 -0400

    Commit database.yml

--- script/pull_latest_production_data.sh
+++ script/pull_latest_production_data.sh
@@ -2,5 +2,5 @@ pg_ctl -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log start
 
 bundle install
 bundle exec rake db:drop RAILS_ENV=development
-heroku pg:pull DATABASE_URL controlled_sub_data_development --app controlled-sub-data
+heroku pg:pull HEROKU_POSTGRESQL_NAVY_URL controlled_sub_data_development --app controlled-sub-data
 bundle exec rails server
